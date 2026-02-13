304b7c4c2400e66c58db4f1780e4326180762674
fitness7de-ux <fitness7de@gmail.com>
2026-02-13 13:49:07 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,63 @@
-motors.dualMotorPower(Motor.M0, 10)
-motors.dualMotorPower(Motor.M1, -10)
+input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    motors.dualMotorPower(Motor.M0_M1, 10)
+    JoyCar.turn(
+    FRDirection.Forward,
+    LRDirection.Left,
+    1,
+    2
+    )
+    while (JoyCar.obstacleavoidance(SensorLRSelection.Left)) {
+        if (JoyCar.obstacleavoidance(SensorLRSelection.Right)) {
+            JoyCar.stop(StopIntensity.Intense)
+        } else {
+            JoyCar.stop(StopIntensity.Soft)
+        }
+    }
+    basic.showLeds(`
+        . . # # .
+        . . # # .
+        # # # # #
+        . . # # .
+        . . # # .
+        `)
+})
+input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . # # # .
+        . . # . .
+        . . . . .
+        `)
+    JoyCar.light(ToggleSwitch.On)
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showLeds(`
+        . . # # .
+        . . # . .
+        . # # # .
+        . . # . .
+        . . # . .
+        `)
+    JoyCar.light(ToggleSwitch.Off)
+    JoyCar.stop(StopIntensity.Soft)
+})
+JoyCar.initJoyCar(RevisionMainboard.OnepThree)
+JoyCar.initController(ControllerType.Calliope)
+basic.showLeds(`
+    . . . . #
+    . . . . #
+    # . # # .
+    . # # # .
+    . . # . .
+    `)
 basic.forever(function () {
 	
 })
