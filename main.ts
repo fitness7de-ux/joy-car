ef7e0c67559bcf9a4076c82b686234d47aa0c2b2
fitness7de-ux <fitness7de@gmail.com>
2026-02-13 14:30:52 +0100

    Aktualisiere main.ts

--- main.ts
+++ main.ts
@@ -13,19 +13,25 @@ input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
     1,
     2
     )
-    while (JoyCar.obstacleavoidance(SensorLRSelection.Left)) {
+    while (0 == 0) {
         if (JoyCar.obstacleavoidance(SensorLRSelection.Right)) {
+            music.play(music.createSoundExpression(WaveShape.Sawtooth, 2154, 1, 255, 0, 1118, SoundExpressionEffect.Vibrato, InterpolationCurve.Logarithmic), music.PlaybackMode.UntilDone)
             JoyCar.stop(StopIntensity.Intense)
-        } else {
-            JoyCar.stop(StopIntensity.Soft)
+            continue;
         }
+        if (JoyCar.obstacleavoidance(SensorLRSelection.Left)) {
+            music.play(music.createSoundExpression(WaveShape.Sawtooth, 2154, 1, 255, 0, 1118, SoundExpressionEffect.Vibrato, InterpolationCurve.Logarithmic), music.PlaybackMode.UntilDone)
+            JoyCar.stop(StopIntensity.Intense)
+            continue;
+        }
+        continue;
     }
     basic.showLeds(`
-        . . # # .
-        . . # # .
+        . . # . .
+        . . # . .
         # # # # #
-        . . # # .
-        . . # # .
+        . . # . .
+        . . # . .
         `)
 })
 input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
@@ -37,8 +43,14 @@ input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
         . . . . .
         `)
     JoyCar.light(ToggleSwitch.On)
+    if (JoyCar.collisionDetection() < 1 && 1 == 1) {
+        music.play(music.createSoundExpression(WaveShape.Sine, 5000, 0, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
+        basic.showNumber(JoyCar.collisionDetection())
+    }
 })
 input.onGesture(Gesture.Shake, function () {
+    JoyCar.light(ToggleSwitch.On)
+    JoyCar.stop(StopIntensity.Soft)
     basic.showLeds(`
         . . # # .
         . . # . .
@@ -46,8 +58,7 @@ input.onGesture(Gesture.Shake, function () {
         . . # . .
         . . # . .
         `)
-    JoyCar.light(ToggleSwitch.Off)
-    JoyCar.stop(StopIntensity.Soft)
+    JoyCar.hazardlights(ToggleSwitch.On)
 })
 JoyCar.initJoyCar(RevisionMainboard.OnepThree)
 JoyCar.initController(ControllerType.Calliope)
